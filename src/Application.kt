f50cc97b48ef18ff66a361b231a4d94ab2d57480
kirimin <minkarakirimade@gmail.com>
2020-03-25 13:27:59 +0900

    first commit

---
+++ src/Application.kt
@@ -0,0 +1,19 @@
+package site.kirimin_chan.board
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
+
